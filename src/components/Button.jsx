650174c23a393490424bf507d954482d851ed8dc
mibrahim234 <mohamm1417@gmail.com>
2022-06-01 23:35:26 -0400

    seting color scheme

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,16 @@
 import './Button.css'
 import {Link} from 'react-router-dom';
 
+const STYLES = ['btn--primary', 'btn--outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
 export const Button = ({
     children,
      type, 
      onClick, 
      buttonStyle, 
-     buttonSize})
-      => {
+     buttonSize
+    })  => {
           const checkButtonStyle = STYLES.inclides(buttonStyle) ? buttonStyle : STYLES[0]
       };
