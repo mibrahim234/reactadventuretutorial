1af3f391e1b47574b71a8bad23510ccabce0f34c
mibrahim234 <mohamm1417@gmail.com>
2022-06-01 23:38:07 -0400

    add return for btn compo

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -14,3 +14,9 @@ export const Button = ({
     })  => {
           const checkButtonStyle = STYLES.inclides(buttonStyle) ? buttonStyle : STYLES[0]
       };
+
+const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+return (
+    <Link to='/sign-up' className=''
+)
