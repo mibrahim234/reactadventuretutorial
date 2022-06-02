3c3d69828698e185457c0a04f9ca8ea3ab0db9cc
mibrahim234 <mohamm1417@gmail.com>
2022-06-01 23:28:16 -0400

    adding list items for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,11 @@ import React, {useState} from 'react'
 import {Link } from 'react-router-dom'
 function Navbar() {
 
-  const {click, setClick} = useState(false); 
+  const [click, setClick] = useState(false); 
+
+  const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
     <nav className="navbar">
@@ -15,6 +19,23 @@ function Navbar() {
       {/* if clicked change icon */}
       <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
     </div>
+    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+      <li className='nav-item'> 
+      <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+        Home
+      </Link>
+      </li>
+      <li className='nav-item'> 
+      <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+        Services
+      </Link>
+      </li>
+      <li className='nav-item'> 
+      <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+        Products 
+      </Link>
+      </li>
+    </ul>
         </div>
     </nav>
     </>
