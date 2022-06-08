e10f43c558459a55097ad7abf63eaaa5a72a38b5
mibrahim234 <mohamm1417@gmail.com>
2022-06-07 23:55:21 -0400

    added imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-import Navbar from './components/Navbar' 
-import { Router, Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
-    <Router>
-    <Navbar/>
-    <Routes>
-      <Route path='/' exact />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+        </Switch>
+      </Router>
     </>
-
   );
 }
 
